6c85b3479dfc0fb3929e79937945ce84289b7110
kuimbae <delgadofleitas@hotmail.com>
2025-05-30 10:28:33 +0200

    creando test de prueba de inicio de sesion

--- Test-Playwright/example.spec.ts
+++ Test-Playwright/example.spec.ts
@@ -23,3 +23,25 @@ test('mostrar genero aventura', async ({ page }) => {
   await page.getByRole('combobox').selectOption('Aventura');
   await page.pause();
 });
+
+test('iniciar sesion', async ({ page }) => {
+  await page.goto('http://localhost:5173/carteleraReact/');
+   
+  // Hacer clic en el enlace que activa el scroll hacia el formulario
+  await page.getByText('Iniciar Sesion', { exact: false }).click();
+
+
+   // Esperar a que el formulario aparezca en pantalla (por placeholder)
+  await expect(page.getByPlaceholder('Tu nombre')).toBeVisible();
+
+  // Rellenar los campos usando los placeholders
+  await page.getByPlaceholder('Tu nombre').fill('usuarioPrueba');
+  await page.getByPlaceholder('ejemplo@email.com').fill('usuario@prueba.com');
+  await page.getByPlaceholder('Tu contraseña').fill('contraseñaSegura123');
+ 
+  // Enviar el formulario
+  await page.getByRole('button', { name: /enviar/i }).click();
+  
+ 
+});
+ 
