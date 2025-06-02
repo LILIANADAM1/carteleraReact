f0543c9bb0138ce478d0cb5aea3cb92732043bcc
Orlando Pineda Raad <orpira@icloud.com>
2025-06-02 06:47:42 +0200

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/", // 👈 este es el cambio clave
+  base: "/lilianadam1.github.io/carteleraReact/", // 👈 este es el cambio clave
   plugins: [react()],
   envPrefix: ["VITE_"],
   test: {
